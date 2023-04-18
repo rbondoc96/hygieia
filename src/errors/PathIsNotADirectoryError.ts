59ef7d07ae359452aad6594a25f30c8c4056c8d2
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-04-18 02:50:57 -0700

    update: utils for checking directories

---
+++ src/errors/PathIsNotADirectoryError.ts
@@ -0,0 +1,9 @@
+export default class PathIsNotADirectoryError extends Error {
+    override readonly name = 'PATH_IS_NOT_A_DIRECTORY_ERROR';
+    override readonly message: string;
+
+    constructor(path: string) {
+        super();
+        this.message = path + ' is not a directory.';
+    }
+}
---
+++ src/utils/dir.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import PathIsNotADirectoryError from '@/errors/PathIsNotADirectoryError';
+import {copyFile} from '@/utils/file';
+
+export function copyDir(srcDir: string, destDir: string) {
+    fs.mkdirSync(destDir, {recursive: true});
+
+    for (const file of fs.readdirSync(srcDir)) {
+        const srcFile = path.resolve(srcDir, file);
+        const destFile = path.resolve(destDir, file);
+        copyFile(srcFile, destFile);
+    }
+}
+
+export function emptyDir(dir: string) {
+    if (!fs.existsSync(dir)) {
+        return;
+    }
+
+    for (const folder of fs.readdirSync(dir)) {
+        if (folder === '.git') {
+            continue;
+        }
+        fs.rmSync(path.resolve(dir, folder), {
+            recursive: true,
+            force: true,
+        });
+    }
+}
+
+export function isDirEmpty(path: string) {
+    if (!fs.existsSync(path)) {
+        return true;
+    }
+
+    if (!fs.lstatSync(path).isDirectory()) {
+        throw new PathIsNotADirectoryError(path);
+    }
+
+    const files = fs.readdirSync(path);
+    return files.length === 0 || (files.length === 1 && files[0] === '.git');
+}
