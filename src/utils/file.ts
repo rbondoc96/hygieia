5874f9a42780fbdd2e8fc73723a48660ca5619b8
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-04-18 02:51:11 -0700

    update: utils for handling files

---
+++ src/utils/file.ts
@@ -0,0 +1,22 @@
+import type {BufferEncoding} from 'typescript';
+import fs from 'fs';
+import {copyDir} from '@/utils/dir';
+
+export function copyFile(src: string, dest: string) {
+    const fileStats = fs.statSync(src);
+
+    if (fileStats.isDirectory()) {
+        copyDir(src, dest);
+    } else {
+        fs.copyFileSync(src, dest);
+    }
+}
+
+export function editFile(
+    file: string,
+    callback: (content: string) => string,
+    encoding: BufferEncoding = 'utf-8',
+) {
+    const contents = fs.readFileSync(file, encoding);
+    fs.writeFileSync(file, callback(contents), encoding);
+}
