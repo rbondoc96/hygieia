f4b7c472e704c9e2a146d31930ac59c32ab3fa73
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-04-19 00:01:40 -0700

    update: generate tsconfig.json with recommended settings (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
+import fs from 'fs';
+import {reset} from 'kolorist';
 import minimist from 'minimist';
 import path from 'path';
 import prompts from 'prompts';
 import {fileURLToPath} from 'url';
-import {formatTargetDir} from './utils';
+import {copyFile, emptyDir, formatTargetDir, isDirEmpty} from './utils';
 import OperationCancelledError from './errors/OperationCancelledError';
+
+const Prompts = {
+    CheckShouldOverwriteDirectory: {
+        key: 'checkShouldOverwriteDirectory',
+    },
+    ProjectName: {
+        key: 'projectName',
+        message: reset('Project name:'),
+    },
+    ShouldOverwriteDirectory: {
+        key: 'shouldOverwriteDirectory',
+        message: (targetDir: string) =>
+            (targetDir === '.'
+                ? 'Current directory'
+                : `Target directory "${targetDir}"`) +
+            ` is not empty. Remove existing files and continue?`,
+    },
+    UseRecommendedTSConfig: {
+        key: 'useRecommendedTSConfig',
+        message: reset(
+            'Install recommended a tsconfig.json file with recommended settings?',
+        ),
+    },
+} as const;
+
+type AnswerKey = (typeof Prompts)[keyof typeof Prompts]['key'];
 
 const defaultProjectName = 'my-project';
 
@@ -20,20 +48,45 @@ export default async function main() {
 
     let targetProjectName = projectNameArg ?? defaultProjectName;
 
-    let results;
+    let results: prompts.Answers<AnswerKey>;
 
     try {
         results = await prompts(
             [
                 {
                     type: projectNameArg !== undefined ? null : 'text',
-                    name: 'projectName',
-                    message: 'Project name:',
+                    name: Prompts.ProjectName.key,
+                    message: Prompts.ProjectName.message,
                     initial: defaultProjectName,
                     onState: state => {
                         targetProjectName =
                             formatTargetDir(state.value) ?? defaultProjectName;
                     },
+                },
+                {
+                    type: () =>
+                        !fs.existsSync(targetProjectName) || isDirEmpty(targetProjectName)
+                            ? null
+                            : 'confirm',
+                    name: Prompts.ShouldOverwriteDirectory.key,
+                    message: Prompts.ShouldOverwriteDirectory.message(targetProjectName),
+                },
+                {
+                    type: (_, previousResults) => {
+                        if (
+                            previousResults[Prompts.ShouldOverwriteDirectory.key] ===
+                            false
+                        ) {
+                            throw new OperationCancelledError();
+                        }
+                        return null;
+                    },
+                    name: Prompts.CheckShouldOverwriteDirectory.key,
+                },
+                {
+                    type: 'confirm',
+                    name: Prompts.UseRecommendedTSConfig.key,
+                    message: Prompts.UseRecommendedTSConfig.message,
                 },
             ],
             {
@@ -51,11 +104,34 @@ export default async function main() {
         return;
     }
 
-    console.log(results);
+    const {
+        [Prompts.ShouldOverwriteDirectory.key]: shouldOverwriteDirectory,
+        [Prompts.UseRecommendedTSConfig.key]: useRecommendedTSConfig,
+    }: {
+        [Prompts.ShouldOverwriteDirectory.key]: boolean;
+        [Prompts.UseRecommendedTSConfig.key]: boolean;
+    } = results;
 
     const projectRoot = path.join(cwd, targetProjectName);
+    const templatesRoot = path.resolve(
+        fileURLToPath(import.meta.url),
+        '../..',
+        'templates',
+    );
+    const recommendedTemplatesRoot = path.resolve(templatesRoot, 'recommended');
 
-    console.log(projectRoot);
+    if (shouldOverwriteDirectory) {
+        emptyDir(projectRoot);
+    } else if (!fs.existsSync(projectRoot)) {
+        fs.mkdirSync(projectRoot, {
+            recursive: true,
+        });
+    }
 
-    console.log(path.resolve(fileURLToPath(import.meta.url), '../..', 'templates'));
+    if (useRecommendedTSConfig) {
+        copyFile(
+            path.join(recommendedTemplatesRoot, 'tsconfig.json'),
+            path.join(projectRoot, 'tsconfig.json'),
+        );
+    }
 }
