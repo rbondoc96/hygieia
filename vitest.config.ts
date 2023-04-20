944ecf6b3094f4fef7991457af417a5657396e83
Rodrigo Bondoc <rbondoc96@gmail.com>
2023-04-20 02:28:24 -0700

    build: add c8 code coverage (#3)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,11 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
     test: {
-        exclude: ['**/node_modules/**', '**/dist/**'],
+        coverage: {
+            provider: 'c8',
+            reportsDirectory: './__tests__/coverage',
+        },
+        exclude: ['**/node_modules/**', '**/dist/**', '**/coverage/**'],
         testTimeout: 10000,
         // node14 often fails with a Segmentation Fault
         threads: !process.versions.node.startsWith('14'),
